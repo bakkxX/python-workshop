fa6559d4945b30eecd492c7e365857a7661ae126
bakkxX <chamathjayasekara2001@gmail.com>
2025-06-07 12:51:46 +0530

    first comm

---
+++ basic_arithmetic.py
@@ -0,0 +1,7 @@
+a = 10
+b = 5
+sum_result = a + b
+difference_result = a - b
+product_result = a * b
+division_result = a / b
+print(f"Sum: {sum_result}, Difference: {difference_result}, Product: {product_result}, Division: {division_result}")
